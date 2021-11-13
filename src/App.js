f5e80bc2f0cae097ada96df37cd6080a802717d7
Vanessa Santana <vanessah4money@gmail.com>
2021-11-13 14:40:06 -0300

    feat: redux com react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Total: {state}</h1>
+      <button onClick={()=> dispatch({type:"INCREMENTAR"})}>Incrementar</button>
     </div>
   );
 }
